489b7149cd806325b8d53c59098f54c9d8892c80
andy reyes <11710reyes@gmail.com>
2018-11-15 02:53:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,12 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
  return object
 } 
  
- function deleteFromObjectByKey(object, key) {
-  var newRecipe = Object.assign({}, object)
-  delete newRecipe.key
-  return newRecipe
-} 
- 
- function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
+function deleteFromObjectByKey(object, key){
+  var newObj =  Object.assign({},object)
+  delete newObj[key]
+  return newObj
  }
+ function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
