2a19f5a805f1b669ba40eb8fb45ab9b830463c2e
andy reyes <11710reyes@gmail.com>
2018-11-15 02:49:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,20 @@ updateObjectWithKeyAndValue(object, flour, '2 cups')
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign ({}, object, {[key] : value})
 }
+
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key] : value})
-}
+ object[key] = value
+ return object
+} 
+ 
+ function deleteFromObjectByKey(object, key) {
+  var newRecipe = Object.assign({}, object)
+  newRecipe // { foo: 'bar' }
+  delete newRecipe.key
+  return newRecipe
+} 
+ 
+ function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key
+  return object
+ }
